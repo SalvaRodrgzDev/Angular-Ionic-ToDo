93f457eb7a8efbbe19f653fcc7bbe88a8090c314
Salvador Ruiz <salvador.ruiz@gus.global>
2020-05-01 21:10:31 +0200

    added /agregar page

---
+++ src/app/pages/agregar/agregar.page.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-agregar',
+  templateUrl: './agregar.page.html',
+  styleUrls: ['./agregar.page.scss'],
+})
+export class AgregarPage {
+
+  constructor() { 
+
+  }
+}
