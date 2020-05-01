7f168b74d5ce85c3265bc314d89f849bc8d0dfb2
Salvador Ruiz <salvador.ruiz@gus.global>
2020-05-01 22:36:33 +0200

    agregar lista funcionalidad añadida

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { TodosService } from 'src/app/services/todos.service';
 import { Lista } from 'src/app/models/lista.model';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -11,11 +12,44 @@ import { Router } from '@angular/router';
 export class Tab1Page {
 
   constructor( public todosService: TodosService,
-                private router: Router) {
+                private router: Router,
+                private alertCtrl: AlertController) {
 
   }
 
-  agregarLista() {
-    this.router.navigateByUrl('/tabs/tab1/agregar');
+  async agregarLista() {
+    //this.router.navigateByUrl('/tabs/tab1/agregar');
+
+    const alert = await this.alertCtrl.create({
+      header: 'nueva lista',
+      inputs: [
+        {
+          name: 'titulo',
+          type: 'text',
+          placeholder: 'Nombre de la lista'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancelar');
+          }
+        },
+        {
+          text: 'Crear',
+          handler: data => {
+            if (data.titulo.length === 0) {
+              return
+            }
+
+            this.todosService.crearLista(data.titulo);
+          }
+        }
+      ]
+    });
+
+    alert.present();
   }
 }
